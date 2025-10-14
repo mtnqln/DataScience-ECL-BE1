c4d008db5a506e69b1df7c42b279c17bc005bb40
Pierre Elie <pierrelielvl@gmail.com>
2025-10-14 12:44:06 +0200

    lines

--- separate_lines.py
+++ separate_lines.py
@@ -1,7 +1,7 @@
 import re
 from collections import Counter
-
-
+from handle_data_pandas import read_ds
+import pandas as pd
 
 
 
@@ -9,9 +9,15 @@ from collections import Counter
 
 if __name__ == "__main__":
 
+    features_train = read_ds("data/train.csv")
+    features_test = read_ds("data/test.csv")
+
     pattern_ecran = re.compile(r"\((.*?)\)")
     pattern_conf_ecran = re.compile(r"<(.*?)>")
     pattern_chaine = re.compile(r"\$(.*?)\$")
     
+    features_train["util"] = pd.Categorical(features_train["util"])
 
-
+    def two_pattern(df):
+        for index, row in df.iterrows():
+            pass
