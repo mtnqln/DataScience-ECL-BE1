666279a816636c99c348064c5b2fd9a3678eb243
Pierre Elie <pierrelielvl@gmail.com>
2025-10-14 11:57:04 +0200

    readme

---
+++ separate_lines.py
@@ -0,0 +1,17 @@
+import re
+from collections import Counter
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    pattern_ecran = re.compile(r"\((.*?)\)")
+    pattern_conf_ecran = re.compile(r"<(.*?)>")
+    pattern_chaine = re.compile(r"\$(.*?)\$")
+    
+
+
