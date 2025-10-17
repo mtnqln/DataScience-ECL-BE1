37472f0421ec093d1e36557df6557880e0f7b02e
mtnqln <martin.quilichini2@gmail.com>
2025-10-17 22:23:34 +0200

    first run at knn

---
+++ tests/models/test_knn.py
@@ -0,0 +1,15 @@
+from src.helper import score_accuracy
+from src.models.knn import knn_inference
+from src.handle_data_pandas import read_ds
+from src.prepare_data_for_model import prepare_data
+
+def test_knn_inference():
+    features_train = read_ds("data/train.csv")
+    X_train,X_test,y_train,y_test = prepare_data(features_train)
+    y_pred = knn_inference(X_train=X_train,Y_train=y_train,X_predict=X_test,number_of_neighbors=8)
+
+    accuracy = score_accuracy(y_test,y_pred)
+    print("Accuracy : ",accuracy)
+    assert accuracy > 0.8
+
+
