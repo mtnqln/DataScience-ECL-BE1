b4ac0405e520b7bfdb68d94ea86c6299e0bced1f
mtnqln <martin.quilichini2@gmail.com>
2025-10-18 16:59:10 +0200

    knn test, best f1 score for k=1 0.6

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import numpy as np
-from src.handle_data_pandas import read_ds
-from src.prepare_data_for_model import prepare_data
+from best_knn import best_knn
 
 def main():
-    features_train = read_ds("data/train.csv")
-    X_train,X_test,y_train,y_test = prepare_data(features_train) 
-    print(X_train.shape)
+    best_knn()
 
 if __name__ == "__main__":
-    main()
+    best_knn()
     
