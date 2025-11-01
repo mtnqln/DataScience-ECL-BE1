49c46ff46d6a7b83736dd192746c08e6f9cc059c
Pierre Elie <pierrelielvl@gmail.com>
2025-11-01 18:08:57 +0100

    change_prepare_data_for_model

--- src/prepare_data_for_model.py
+++ src/prepare_data_for_model.py
@@ -3,6 +3,7 @@ from .get_stat_from_data import (
     browsers_per_player,
     get_actions_frequency,
     get_mean_time,
+    get_consecutive_action_tuples
 )
 from .helper import normalize_df
 from sklearn.preprocessing import normalize,LabelEncoder
@@ -20,9 +21,15 @@ def prepare_data_for_prediction(df_train:pd.DataFrame, df_test:pd.DataFrame|None
     mean_time = normalize_df(get_mean_time(df=df),not_cols=0).drop(0,axis=1)
     browsers_p_player = normalize_df(browsers_per_player(df=df,normalize=True,unique=False),not_cols=0).drop(0,axis=1)
     actions_frequency = normalize_df(get_actions_frequency(df=df),not_cols=0)
+    tuples_consecutives_action = normalize_df(get_consecutive_action_tuples(df=df),not_cols=0).drop(0,axis=1)
 
-    df_buff = pd.merge(actions_frequency,browsers_p_player,left_index=True,right_index=True)
-    df_processed = pd.merge(df_buff,mean_time,left_index=True,right_index=True)
+    # For labels
+    y = actions_frequency[0]
+    actions_frequency = actions_frequency.drop(0,axis=1)
+    mean_time = normalize_df(get_mean_time(df=df),not_cols=0).drop(0,axis=1)
+    df_buff1 = pd.merge(actions_frequency,browsers_p_player,left_index=True,right_index=True)
+    df_buff2 = pd.merge(df_buff1,mean_time,left_index=True,right_index=True)
+    df_processed = pd.merge(df_buff2,tuples_consecutives_action, left_index=True,right_index=True)
     
     Y_train = df_processed[df_processed[0]!="a"][0]
     X_train = df_processed[df_processed[0]!="a"].drop(0,axis=1)
